97ad41ab5ce04220e7141a95abc25a4942232f7d
ENG9MANAR9SC <manarfajralkherir@gmail.com>
2023-04-06 15:04:12 +0300

    add some features to result search

--- src/components/searchresult.vue
+++ src/components/searchresult.vue
@@ -1,33 +1,35 @@
 <template>
-    <div class="c-container mt-16">
+    <div class="c-container mt-16 mb-16">
         <h2 class="h-header">The Results</h2>
         <div class="h-0.5 bg-secondery my-2"></div>
-        <div class="h-description">
-          {{ result }}
+        <div class="c-container">
+            <div v-for="result in results" class="responsivefour">
+                <productCard :key="result.title" :product="result"  class="mx-2"/> 
+            </div>
         </div>
-        <div v-for="result in result" class="responsivefour">
-                <!-- {{ product }} -->
-                <productCard :result="result" class="mx-2"/>
-        </div>
+       
     </div>
 </template>
 
 <script>
 import { products } from "../assets/data"; 
-import productCard from "../products/card.vue";
+import productCard from "../components/products/card.vue";
  export default {
   data() {
         return { 
             products: [],
             searchKey:'',
-            result:[],
+            results:[],
+            product: [],
         }
     },
     mounted() {
         console.log(this.$route.query.s)
         this.searchKey = this.$route.query.s;
         this.products = products;
-        this.result = products.filter(el => el.title.toLowerCase().search(this.searchKey.toLowerCase())!=-1)
+        this.results = products.filter(el => el.title.toLowerCase().search(this.searchKey.toLowerCase())!=-1)
+        this.product= products.filter(el => el.title.toLowerCase().search(this.searchKey.toLowerCase())!=-1)
+
     },
     components: {
         productCard,
