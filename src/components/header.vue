a10f5abb19c720ca0ca6ea678740438cb95baa38
ENG9MANAR9SC <manarfajralkherir@gmail.com>
2023-04-10 15:49:41 +0300

    fix bugs

--- src/components/header.vue
+++ src/components/header.vue
@@ -10,8 +10,8 @@
 
                     <div class="flex items-center ">
                         <div class="mx-6">
-                           <RouterLink to="/register" ><button class="mx-2 border-white border py-2 px-3 rounded-lg">Register</button></RouterLink> 
-                           <RouterLink to="/login" ><button class="mx-2 border-white border py-2 px-3 rounded-lg">Sign in</button></RouterLink>
+                           <RouterLink to="/register" ><button class="mx-2 py-2 px-2 rounded-lg font-tenor">{{ $t('Register') }} </button></RouterLink> 
+                           <RouterLink to="/login" ><button class="mx-2  py-2 px-2 rounded-lg font-tenor">{{ $t('Log in') }}</button></RouterLink>
                         </div>
                         <dropdown class="sm:block hidden"></dropdown>
                         
@@ -27,33 +27,21 @@
                     </div>
 
                     <!-- list Desktop & mobile -->
-                    <nav class="flex items-center justify-between flex-wrap p-6">
-                        <!-- <div class="flex items-center flex-shrink-0 text-secondery mr-6">
-                            <i class="fas fa-shopping-cart fa-2x"></i>
-                            <span class=" text-xl tracking-tight ml-2">My Store</span>
-                        </div> -->
-                        <div class="block lg:hidden">
+                    <nav class="flex items-center justify-between flex-wrap p-6" >
+                        <div class="block lg:hidden" >
                             <button class="flex items-center px-3 py-2 border rounded text-secondery border-white hover:text-gray-200 hover:border-gray-200" @click="toggleMenu">
-                                <!-- <i class="fas fa-bars"></i> -->
-                                <img src="/icons/menu.png" alt="" class="h-5 w-5" @click="toggleMenu">
+                                <i class="fas fa-bars" ></i>
+                                <img src="/icons/menu.png" alt="" class="h-5 w-5" >
                             </button>
                         </div>
                         <div :class="{ 'hidden': !showMenu }" class="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
                             <div class="text-lg lg:flex-grow">
                                 <RouterLink  class="block mt-4 lg:inline-block lg:mt-0 text-secondery mx-2 p-2 hov-menu" to="/">{{ $t('Home') }}  </RouterLink>
                                 <RouterLink class="block mt-4 lg:inline-block lg:mt-0 text-secondery hover:text-gray-200 mx-2 p-2 hov-menu" to="/product"> {{ $t('Shop') }} </RouterLink>
-                                <div class="relative mt-4 lg:hidden">
-                                    <button class="text-secondery hover:text-gray-200 focus:outline-none focus:shadow-outline mx-2 p-2 hov-menu" @click="showCategories = !showCategories">
-                                        <span>Categories</span>
-                                        <i :class="{ 'fa-angle-down': !showCategories, 'fa-angle-up': showCategories }" class="fas ml-2"></i>
-                                    </button>
-                                    <div :class="{ 'hidden': !showCategories }" class="absolute right-0 mt-2 py-2 w-48 bg-white rounded-lg shadow-xl z-10">
-                                      <!--   <a href="#" class="block px-4 py-2 text-gray-800 hover:bg-gray-100 hover:text-gray-900"> Category 1  </a> -->
-                                    </div>
-                                </div>
-                                    <RouterLink class="block mt-4 lg:inline-block lg:mt-0 text-secondery mx-2 p-2 hov-menu" to="/blog">{{ $t('Blog') }} </RouterLink>
-                                    <RouterLink class="block mt-4 lg:inline-block lg:mt-0 text-secondery mx-2 p-2 hov-menu" to="/contact-us">{{ $t('Contact us') }} </RouterLink>
-                                    <RouterLink class="block mt-4 lg:inline-block lg:mt-0 text-secondery mx-2 p-2 hov-menu" to="/about-us">{{ $t('About us') }}  </RouterLink>
+                                <RouterLink class="block mt-4 lg:inline-block lg:mt-0 text-secondery mx-2 p-2 hov-menu" to="/Categories">{{ $t('Categories') }} </RouterLink>
+                                <RouterLink class="block mt-4 lg:inline-block lg:mt-0 text-secondery mx-2 p-2 hov-menu" to="/blog">{{ $t('Blog') }} </RouterLink>
+                                <RouterLink class="block mt-4 lg:inline-block lg:mt-0 text-secondery mx-2 p-2 hov-menu" to="/contact-us">{{ $t('Contact us') }} </RouterLink>
+                                <RouterLink class="block mt-4 lg:inline-block lg:mt-0 text-secondery mx-2 p-2 hov-menu" to="/about-us">{{ $t('About us') }}  </RouterLink>
                             </div>
                         </div>
                     </nav>
@@ -61,7 +49,7 @@
                     <div class="flex gap-3 items-center">
                         <search> </search>
                         <RouterLink to="/cart"><img src="/icons/shophead.png" alt="" class="w-8 h-8 p-1" ></RouterLink>
-                        <div>{{ cart.cQuantity }}</div>
+                         <div>{{ cart.cQuantity }}</div> 
                     </div>
 
                 </div>
@@ -74,22 +62,23 @@
 import { ref } from 'vue';
 import search from './utilities/search.vue'
 import dropdown from "./utilities/lang-dropdown.vue"
+/*  import { CartStore } from '../stores/cart'*/
+/*  const cart = CartStore()*/
 export default{
        components: {
         dropdown,
         search,
     },
     setup() {
-    const showMenu = ref(false);
-    const showCategories = ref(false);
-    const toggleMenu = () => {
+/*     const showMenu = ref(false);
+     const toggleMenu = () => {
       showMenu.value = !showMenu.value;
+      
       console.log('do')
-    };
+    };  */
 
     return {
       showMenu,
-      showCategories,
       toggleMenu,
     };
 
@@ -102,8 +91,13 @@ export default{
 import { CartStore } from '../stores/cart'
 
 const cart = CartStore()
-
-</script>
+const showMenu = ref(false);
+     const toggleMenu = () => {
+      showMenu.value = !showMenu.value;
+      
+      console.log('do')
+    }; 
+</script> 
 <style>
 .hov-menu {
    @apply hover:bg-secondery-rose hover:rounded-2xl hover:scale-105 duration-300 delay-150  open:bg-primary-orange
--- src/components/utilities/register.vue
+++ src/components/utilities/register.vue
@@ -1,12 +1,12 @@
 <template>
-    <form @submit.prevent="addUser">
+    <form >
         <div class="c-container">
-                    <div class="mt-16 mb-8 p-0.5 w-3/4  custom-round custom-bgcolor">
+                    <div class="mt-16 mb-8 p-0.5 w-3/4 custom-round custom-bgcolor">
                         <div class="bg-white custom-round p-8">
                             <h2  class="h-header text-2xl mb-4">{{ $t('Register') }} </h2>
-                            <form class="w-full" @submit="addUser">
-                                <div class=" gap-4 ">
-                                    <div class="my-4 ">
+                            <form class="w-full" @submit="handleSignUp">
+                                <div class="gap-4">
+                                    <div class="my-4">
                                         <label class="text-primary font-tenor text-xl" for="full-name">{{ $t('Full Name') }} </label>
                                         <input
                                             class="appearance-none border rounded-xl border-secondery w-full py-3 px-3 text-primary leading-tight focus:outline-none focus:shadow-outline"
@@ -14,11 +14,11 @@
                                             type="text"
                                             :placeholder="$t('your Name')"
                                             v-model="username"
-                                            required
+                                          
                                         />
                                     </div>
 
-                                    <div class="my-4 ">
+                                    <div class="my-4">
                                         <label class="text-primary font-tenor text-xl" for="email">
                                             {{ $t('Email') }} 
                                         </label>
@@ -44,7 +44,7 @@
                                         />
                                 </div>
                             </div>
-                                <div class="flex justify-end  my-4">
+                                <div class="flex justify-end my-4">
                                     <button
                                         class="btn hover:bg-blue-700 text-white focus:outline-none focus:shadow-outline"
                                         type="submit"
@@ -60,40 +60,23 @@
   </template>
   
   <script>
-  import axios from 'axios';
+  import { useUserStore } from '../../stores/user'
+  import { ref } from 'vue';
   export default {
-    data() {
-      return {
-        username: '',
-        email: '',
-        password: '',
-        users:[],
-      };
+    setup() {
+      const userStore = useUserStore()
+  
+      const email = ref('')
+      const password = ref('')
+  
+      const handleSignUp = async () => {
+        await userStore.signUp({ email: email.value, password: password.value })
+        // redirect to the login page
+        console.log('do')
+        router.push('/login')
+      }
+  
+      return { email, password, handleSignUp }
     },
-    methods: {   
-        async addUser() {
-            try {
-                const response = await axios.post('https://fakestoreapi.com/users', {
-                username: this.username,
-                email: this.email,
-                password: this.password,
-                });
-                console.log(response.data);
-            } catch (error) {
-                console.error(error);
-            }
-        },
-       
-    },
-    mounted() {
-         axios.get('https://fakestoreapi.com/users')
-        .then(response => {
-             this.users = response.data;
-             console.log(response)
-         })
-         .catch(error => {
-            console.log(error);
-         })
-    },
-  };
+  }
   </script>
